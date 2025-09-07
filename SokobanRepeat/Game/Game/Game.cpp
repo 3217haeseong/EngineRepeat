7b8202b5974662b5c00790370758cbb93bd2a83c
3217haeseong <gotjd3546@gmail.com>
2025-09-07 22:06:26 +0900

    2025-09-07 10PM

--- SokobanRepeat/Game/Game/Game.cpp
+++ SokobanRepeat/Game/Game/Game.cpp
@@ -2,18 +2,53 @@
 #include "Level/MenuLevel.h"
 #include "Level/SokobanLevel.h"
 
+Game* Game::instance = nullptr;
+
 Game::Game()
 {
+	instance = this;
+	
+	AddLevel(new SokobanLevel());
+
+	menuLevel = new MenuLevel();
 }
 
 Game::~Game()
 {
+	CleanUp();
 }
 
 void Game::ToggleMenu()
 {
+	system("cls");
+
+	showMenu = !showMenu;
+
+	if (showMenu)
+	{
+		backLevel = mainLevel;
+
+		mainLevel = menuLevel;
+	}
+	else
+	{
+		mainLevel = backLevel;
+	}
 }
 
 void Game::CleanUp()
 {
+	if (showMenu)
+	{
+		SafeDelete(backLevel);
+		mainLevel = nullptr;
+	}
+
+	SafeDelete(menuLevel);
+	Engine::CleanUp();
+}
+
+Game& Game::Get()
+{
+	return *instance;
 }
